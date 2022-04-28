8f9a7022144351c401467622ecaaa078e8c8d7ed
bhumikasha <64906380+bhumikasha@users.noreply.github.com>
2022-04-28 17:04:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LineChart from './LineChart/lineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <LineChart></LineChart>
+    </div>
+  );
+}
+
+export default App;
